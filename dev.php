27fe07a038aa2f68def8daadb01db526ef76a9ec
Lewis <mudaidalewis@gmail.com>
2025-01-28 14:57:15 +0300

    This is the best design

---
+++ dev.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The developer</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="CSS/dev.css">
+</head>
+<body>
+   
+    <img src="IMG/758X.gif" alt="">
+   
+    <div class="name">
+        <marquee behavior="scroll" direction="left" scrollamount="10">Mr.H4CK3R coffe break moods☕</marquee>
+    </div>
+    <section>
+      
+        <div class="devsinfo">
+        <header>
+         <div class="back">
+            <a href="index.html"><i class="fa-solid fa-backward"></i></a>
+            go back
+         </div>
+        <h2>About Developer</h2>
+        </header>
+         <div class="para">
+         <span>
+        The developer of the mini CTF project is a skilled full-stack developer with a passion for teaching and innovation. Combining technical expertise with creativity, he crafts engaging challenges that include hidden directories, music playback, and humorous elements. With a solid background in PHP, Linux, and SQL, he brings unique problem-solving experiences to his work. Known for his dedication to mentoring, he inspires others through his hands-on approach to learning and enthusiasm for cybersecurity. His projects reflect a commitment to pushing boundaries and sharing knowledge. This innovative developer is none other than <strong>Mr.H4CK3R.</strong>
+</span>
+   
+         </div>
+         <img src="IMG/4643176.jpg" alt=""  id="hacker">
+        </div>
+        <form action="">
+            <p>Talk to Mr.H4CK3R😎</p>
+            <input type="email"  placeholder="email...." required>
+            <textarea name="" id="" placeholder="message goes here ..."></textarea>
+            <input type="submit" value="send">
+        </form>
+    </section>
+</body>
+</html>
