4ff3808629b85c6508280e4b351df9a5f8327754
Lewis <mudaidalewis@gmail.com>
2025-02-03 16:39:53 +0300

    The changes have been added

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -3,3 +3,16 @@ window.onload = function(){
     var audio = document.getElementById('background-music');
     audio.muted = false;
 }
+
+
+const menu = document.getElementById('test');
+const form = document.getElementById('frm');
+
+menu.onclick = function(){
+
+form.classList.add('frm-clicked')
+
+  
+    
+}
+
