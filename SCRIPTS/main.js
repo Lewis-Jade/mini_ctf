a7148145689f7132bf2d98d34e68e806e765e752
Lewis <mudaidalewis@gmail.com>
2025-02-04 14:48:55 +0300

    bug fixed

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -7,10 +7,19 @@ window.onload = function(){
 
 const menu = document.getElementById('test');
 const form = document.getElementById('frm');
+const exit = document.getElementById('exit');
 
 menu.onclick = function(){
 
 form.classList.add('frm-click')
+
+
+    
+}
+exit.onclick = function(){
+
+
+    form.classList.remove('frm-click')
 
 
     
