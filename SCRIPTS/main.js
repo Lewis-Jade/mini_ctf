b3bdfc77d645f2cc6ac95eb1f10ebd891bd6f541
Lewis <mudaidalewis@gmail.com>
2025-02-05 13:18:00 +0300

    music toggle button added

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -3,7 +3,23 @@ window.onload = function(){
     var audio = document.getElementById('background-music');
     audio.muted = false;
 }
+// audio controll
+var music = document.getElementById('background-music');
+const audioIcon = document.getElementById('audio');
 
+audioIcon.onclick = function () {
+    if (music.paused) { // Check if the music is paused
+        music.play();
+        audioIcon.src = "./IMG/no-audio.png"; // Change to "muted" icon
+    } else {
+        music.pause();
+        audioIcon.src = "./IMG/audio.png"; // Change to "sound on" icon
+    }
+};
+
+
+
+//=========================
 
 const menu = document.getElementById('test');
 const form = document.getElementById('frm');
