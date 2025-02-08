6c56c7b912a81c05e94d096f9a4eeb26ee63e5ee
Lewis <mudaidalewis@gmail.com>
2025-02-08 14:15:10 +0300

    siste ni msee

--- flag.php
+++ flag.php
@@ -6,15 +6,19 @@ $correct_flag = "cyb3rs3c{C0ff33_1s_n0t_Fr33}";
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $submitted_flag = trim($_POST["flag"]);
+    $submitted_flag = trim($_POST["flag-input"]);
     
     if ($submitted_flag === $correct_flag) {
         $_SESSION["message"] = "✅ Congratulations! You found the correct flag!";
+        header("Location: success.html");
+        exit();
+
     } else {
         $_SESSION["message"] = "❌ Incorrect flag! Try again.";
+        header("Location: fail.html");
+        exit();
     }
     
     header("Location: index.php");
     exit();
 }
-?>
